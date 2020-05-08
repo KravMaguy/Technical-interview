1c329f870391c4139152d08a0f95eec1d721bce2
KravMaguy <37171082+KravMaguy@users.noreply.github.com>
2020-05-07 20:10:49 -0500

    insertion

---
+++ Algorithms/Insertion.js
@@ -0,0 +1,15 @@
+const insert=(arr)=>{
+    let length=arr.length
+    for(let i=1;i<length;i++){
+        for(let j=0;j<i;j++){
+            if(arr[j]>arr[i]){
+                let holder=arr.splice(i,1)
+                arr.splice(j,0,holder)
+
+            }
+        }
+    }
+    return arr
+}
+
+insert([6,1,8.9,-3,500,2,89,92,17])
