d13d5f96593a0622956da535505b04d36cea7831
unknown <greykrav@gmail.com>
2020-08-08 03:26:41 -0500

    returning unique pairs but 2 slow

--- Leet/3Sum.js
+++ Leet/3Sum.js
@@ -1,12 +1,26 @@
-https://stackoverflow.com/questions/47255382/removing-duplicate-sub-arrays
+const threeSum=(array, target)=>{
+    let length=array.length
+    let holder=[]
+    for(let i=0;i<length-2;i++){
+        for(let j=i+1;j<length-1;j++){
+            for(let k=j+1;k<length;k++){
+                let a,b,c;
+                a=array[i]
+                b=array[j]
+                c=array[k]
+                if(a+b+c===target){
+                    holder.push([a,b,c].sort())
+                }
+            }
+        }
+    }
+    // return holder
+    const setArray = new Set(holder.map(x => JSON.stringify(x)))
+    const uniqArray = [...setArray].map(x => JSON.parse(x))
+    return uniqArray;
+}
+console.log(threeSum([2,4,3,-2,0,1,-1,0],0))
 
-const arr = [[1,3],[2,5],[1,3],[2,5]];
-
-const setArray = new Set(arr.map(x => JSON.stringify(x)))
-
-const uniqArray = [...setArray].map(x => JSON.parse(x))
-
-console.log(uniqArray)
 
 // Conver the 2D array into 1D array with stringified elements
 
