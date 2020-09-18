4bc48c74f4b169ac63e6fe4a60d249084500c4d6
unknown <greykrav@gmail.com>
2020-09-18 16:58:33 -0500

    binaryDfs

--- FCC solutions/Algorithms Fcc/tree/binaryDfs.js
+++ FCC solutions/Algorithms Fcc/tree/binaryDfs.js
@@ -1,8 +1,40 @@
 const add = require('./add')
-const {displayTree, BinarySearchTree, Node} = add;
-let myTree=new BinarySearchTree()
-myTree.add(1)
-myTree.add(2)
-myTree.add(3)
-myTree.add(-1)
-displayTree(myTree)
+const { displayTree, BinarySearchTree, Node } = add;
+let myTree = new BinarySearchTree()
+// myTree.add(1)
+// myTree.add(2)
+// myTree.add(3)
+// myTree.add(-1)
+// displayTree(myTree)
+
+class treeWithDfs extends BinarySearchTree {
+    // constructor() {
+    //     super();
+    //     this.root = null;
+    // }
+    inOrder() {
+        if (this.root == null) {
+            return null;
+        } else {
+            let result = [];
+            function traverseInOrder(node) {
+                node.left && traverseInOrder(node.left);
+                result.push(node.value);
+                node.right && traverseInOrder(node.right);
+            }
+            traverseInOrder(this.root);
+            return result;
+        };
+    }
+}
+const TreeWithDfs = new treeWithDfs()
+TreeWithDfs.add(18);
+TreeWithDfs.add(-10);
+TreeWithDfs.add(22);
+TreeWithDfs.add(2);
+TreeWithDfs.add(7);
+TreeWithDfs.add(-5);
+
+displayTree(TreeWithDfs)
+console.log('inOrder: ' + TreeWithDfs.inOrder());
+
