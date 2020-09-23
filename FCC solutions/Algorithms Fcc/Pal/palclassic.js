aaaa40ea2f21739f2abf44a117349cb903353758
unknown <greykrav@gmail.com>
2020-09-23 03:16:28 -0500

    pass

--- FCC solutions/Algorithms Fcc/Pal/palclassic.js
+++ FCC solutions/Algorithms Fcc/Pal/palclassic.js
@@ -1,9 +1,9 @@
 function palindrome(str) {
     let i,rev=''
-    str = str.toUpperCase().replace(/\W/g, '')
+    str = str.toUpperCase().replace(/[_\W+]/g, '')
     for(i=str.length-1;i>=0;i--){
         rev+=str[i]
     }
     return rev===str
 }
-palindrome("My age is 0, 0 si ega ym.");
+console.log(palindrome("My age is 0, 0 si ega ym."));
