42bfc91a648fda9834081dd5d00893907b543311
unknown <greykrav@gmail.com>
2020-12-09 03:41:52 -0600

    Remove the root in tree w2 nodes set 2nd 2 be root

--- Data Structures/removeNodes.js
+++ Data Structures/removeNodes.js
@@ -38,53 +38,64 @@ function BinarySearchTree() {
     }
     this.remove = function (data) {
         let rootNode = this.root
-        if (!this.root) { return null }
-        if (data === rootNode.value && !rootNode.left && !rootNode.right) {
-            return this.root = null
-        }
-        let prev;
-        const recurFind = (curr) => {
-            if (curr === null) {
-                console.log(curr, "curr")
-                return null
+        if (!this.root) {
+            return null         if (data === rootNode.value && ((!rootNode.left && rootNode.right) || (rootNode.right && !rootNode.left))) {
+                console.log("reached here ")
+                if (rootNode.left) {
+                    console.log('reached 1')
+                    return this.root = rootNode.left
+                } else {
+                    console.log('reached 2')
+                    return this.root = rootNode.right
+                }
             }
-            if (data > curr.value) {
-                prev = curr
-                curr = curr.right
-                recurFind(curr)
-            } else if (data < curr.value) {
-                prev = curr
-                curr = curr.left
-                recurFind(curr)
-            } else if (data === curr.value) {
-                console.log(prev, 'prev')
-                //test for a case with one child
-                if (!curr.left && !curr.right) {
-                    if (prev.right.value === data) {
-                        prev.right = null
-                    } else {
-                        prev.left = null
-                    }
-                } else if ((!curr.left && curr.right) || (curr.left && !curr.right)) {
-                    if (!curr.left) {
-                        prev.right = curr.right
-                    } else {
-                        prev.left = curr.left
+            if (data === rootNode.value && !rootNode.left && !rootNode.right) {
+                return this.root = null
+            }
+            let prev;
+            const recurFind = (curr) => {
+                if (curr === null) {
+                    console.log(curr, "curr")
+                    return null
+                }
+                if (data > curr.value) {
+                    prev = curr
+                    curr = curr.right
+                    recurFind(curr)
+                } else if (data < curr.value) {
+                    prev = curr
+                    curr = curr.left
+                    recurFind(curr)
+                } else if (data === curr.value) {
+                    console.log(prev, 'prev')
+                    //test for a case with one child
+                    if (!curr.left && !curr.right) {
+                        if (prev.right.value === data) {
+                            prev.right = null
+                        } else {
+                            prev.left = null
+                        }
+                    } else if ((!curr.left && curr.right) || (curr.left && !curr.right)) {
+                        if (!curr.left) {
+                            prev.right = curr.right
+                        } else {
+                            prev.left = curr.left
+                        }
                     }
                 }
             }
+            recurFind(rootNode)
+
         }
-        recurFind(rootNode)
-
     }
-}
 
-let t = new BinarySearchTree()
-t.add(5);
-t.add(3);
-t.add(7);
-t.add(6);
-t.add(10);
-t.add(12);
-t.remove(10)
-displayTree(t)
+    let t = new BinarySearchTree()
+    // t.add(5);
+    // t.add(3);
+    // t.add(7);
+    // t.add(6);
+    t.add(10);
+    t.add(12);
+    t.remove(10)
+
+    displayTree(t)
