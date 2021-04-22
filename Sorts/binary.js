66abc0706b6ebbda5a9dd6e2f98a2f053dd70a0e
krav maguy <flex4lease@gmail.com>
2021-04-21 19:12:55 -0500

    refacor

--- Sorts/binary.js
+++ Sorts/binary.js
@@ -1,10 +1,10 @@
 const arr=[1,2,3,4,5,6,7,8,9,10]
 
 const findElem=(array, elem)=>{
-  if(elem<array[0]||elem>array[array.length-1]){
+  let min=0, max=array.length-1
+  if(elem<array[min]||elem>array[max]){
     return console.log('not in array')
   }
-  let min=0, max=array.length-1
   while(min<=max){
     let middle=Math.floor((max+min)/2)
     if(array[middle]>elem){
